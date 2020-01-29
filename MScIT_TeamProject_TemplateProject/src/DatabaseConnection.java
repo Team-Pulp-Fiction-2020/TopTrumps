0ed876a892e7c40732cbbcdadbaf63b6c44fa56e
Emily Quimby <9906376q@student.gla.ac.uk>
2020-01-29 14:16:50 +0000

    database stuff

--- MScIT_TeamProject_TemplateProject/src/DatabaseConnection.java
+++ MScIT_TeamProject_TemplateProject/src/DatabaseConnection.java
@@ -5,8 +5,10 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class DatabaseConnection {
- public static void main(String[] args) {
+ private int gamesPlayed = 0;
 
+public DatabaseConnection() throws SQLException {
+	
   //load the JDBC driver
   try {
    Class.forName("org.postgresql.Driver");
@@ -22,6 +24,9 @@ public class DatabaseConnection {
   //proceed with a database connection
   Connection connection = null;
   // connect to the yacata.dcs.gla.ac.uk server, on port:5432
+  Statement statement1 = null;
+  String sql1 = "SELECT COUNT(GameID) AS Total_No_Games FROM Games;";
+//  String sql2 =
 
   try {
    connection = DriverManager.getConnection("jdbc:postgresql://yacata.dcs.gla.ac.uk:5432/",
@@ -37,6 +42,12 @@ public class DatabaseConnection {
   if (connection != null) {
    try {
     System.out.println("Controlling your database!");
+    statement1 = connection.createStatement();
+    ResultSet resultGamesPlayed = statement1.executeQuery(sql1);
+    while(resultGamesPlayed.next()) {
+    	gamesPlayed = resultGamesPlayed.getInt(1);
+    }
+ 
     //do not forget to close the connection to your database!
 
     connection.close();
@@ -48,7 +59,11 @@ public class DatabaseConnection {
    System.out.println("Failed to establish connection.");
   }
   //if-else
+  
  }
 
+public int getGamesPlayed() {
+	return gamesPlayed;
+}
 
 }
--- MScIT_TeamProject_TemplateProject/src/TopTrumps.java
+++ MScIT_TeamProject_TemplateProject/src/TopTrumps.java
@@ -1,17 +1,36 @@
+import java.sql.SQLException;
+import java.util.Scanner;
+
 import commandline.TopTrumpsCLIApplication;
 
 import online.TopTrumpsOnlineApplication;
 
 
-
 public class TopTrumps {
 
 	/** This is the main class for the TopTrumps Applications */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException{
+		final DatabaseConnection connection = new DatabaseConnection();
+		Scanner scanner = new Scanner(System.in);
+		
+	//	int SQLGames = connection.executeQuery("SELECT GamesPlayed FROM Player WHERE PlayerID = 0");
 		
 		System.out.println("--------------------");
 		System.out.println("--- Top Trumps   ---");
 		System.out.println("--------------------");
+		
+		System.out.println("\nDo you want to see past results or play a game?\n\t\t"
+				+ "1: Print Game Statistics\n\t\t2: Play Game\n"
+				+ "Enter the number for your selection:");
+		try{ if (scanner.nextInt() == 1) {
+				System.out.println("\n\n\nGame Statistics:\n"
+						+ "\t\t\tNumber of Games:" + connection.getGamesPlayed());
+			}else {
+			//playGame()
+		}
+			}catch (Exception e) {
+			System.out.println("please enter either 1 or 2");
+		}
 		
 		// command line switches
 		boolean onlineMode = false;
@@ -27,7 +46,7 @@ public class TopTrumps {
 			
 		}
 		
-		// We cannot run online and command line mode simultaniously
+		// We cannot run online and command line mode simultaneously
 		if (onlineMode && commandLineMode) {
 			System.out.println("ERROR: Both online and command line mode selected, select one or the other!");
 			System.exit(0);
