855202b67b1d8aef374e14af3b217afc4adc2e84
Francescakennedy <59996774+Francescakennedy@users.noreply.github.com>
2020-02-07 13:07:30 +0000

    Update TopTrumpsCLIApplication.java

--- TopTrumps/TopTrumps-master/MScIT_TeamProject_TemplateProject/src/commandline/TopTrumpsCLIApplication.java
+++ TopTrumps/TopTrumps-master/MScIT_TeamProject_TemplateProject/src/commandline/TopTrumpsCLIApplication.java
@@ -8,36 +8,37 @@ import java.util.Scanner;
  */
 public class TopTrumpsCLIApplication {
 
-	/**
-	 * This main method is called by TopTrumps.java when the user specifies that they want to run in
-	 * command line mode. The contents of args[0] is whether we should write game logs to a file.
- 	 * @param args
-	 * @throws SQLException 
-	 */
-	public static void main(String[] args) throws SQLException {
-		final DatabaseConnection connection = new DatabaseConnection();
-		Scanner scanner = new Scanner(System.in);
+    /**
+     * This main method is called by TopTrumps.java when the user specifies that they want to run in
+     * command line mode. The contents of args[0] is whether we should write game logs to a file.
+     * @param args
+     * @throws SQLException 
+     */
+    public static void main(String[] args) throws SQLException {
+        final DatabaseConnection connection = new DatabaseConnection();
+        Scanner scanner = new Scanner(System.in);
 
-		boolean writeGameLogsToFile = false; // Should we write game logs to file?
-		//if (args[0].equalsIgnoreCase("true")) writeGameLogsToFile=true; // Command line selection
-		
-		// State
-		boolean userWantsToQuit = false; // flag to check whether the user wants to quit the application
-		
-		// Loop until the user wants to exit the game
-		while (!userWantsToQuit) {
+        boolean writeGameLogsToFile = false; // Should we write game logs to file?
+        //if (args[0].equalsIgnoreCase("true")) writeGameLogsToFile=true; // Command line selection
 
-			// ----------------------------------------------------
-			// Add your game logic here based on the requirements
-			// ----------------------------------------------------
-			Controller controller = new Controller();
-			controller.play();
-		}
-			userWantsToQuit=true; // use this when the user wants to exit the game
-			
-		}
+        // State
+        boolean userWantsToQuit = false; // flag to check whether the user wants to quit the application
+
+        // Loop until the user wants to exit the game
+        while (!userWantsToQuit) {
+
+            // ----------------------------------------------------
+            // Add your game logic here based on the requirements
+            // ----------------------------------------------------
+            Controller controller = new Controller();
+            controller.play();
+        }
+        userWantsToQuit = true; // use this when the user wants to exit the game
+
+    }
 
 
-	}
+}
 
+}
 
